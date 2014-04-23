8050835cb9c8d158db107a0b47cdc41a99824b81
vinyaa <kmm137@pitt.edu>
2014-04-23 19:38:15 -0400

    Revert "Update Google API, Study JSONs"
    This reverts commit 05d337454bbe6487547f8cd2388c3baac29a2e74.

--- web/submitStudy.php
+++ web/submitStudy.php
@@ -11,14 +11,4 @@ $pay_val = $_POST["pay_value"];
 $keywords = $_POST["keyword"];
 $descr = $_POST["description"];
 
-$age = $_POST["age_elig"];
-$gender = $_POST["gender_elig"];
-$lang = $_POST["lang_elig"];
-$vision = $_POST["vision_elig"];
-$edu = $_POST["education_elig"];
-$exp = $_POST["experience_elig"];
-$program = $_POST["exp_prog"];
-$cit = $_POST["cit_elig"];
-$other = $_POST["other_input"];
-
 ?>
