2cc58cd194183b9e34e18b439df0544b96815368
AndersonRich <lordofthewyrms@gmail.com>
2014-04-25 17:56:41 -0400

    recom

--- scripts/editStudy.js
+++ scripts/editStudy.js
@@ -157,11 +157,11 @@ function displayStudy(jsonObj)
     $.each(val, function(v) {
       $("input[name="+key+"][value="+v+"]").attr("checked",true);
     });
-    //if(val.length>0) {
-    //  var n = key.split("_");
-     // n = n[n.length-1].toLowerCase();
-     // $("#" + n + "_count").html(val.length + " restrictions in " + n + " category");
-    //}
+    if(val.length>0) {
+      var n = key.split("_");
+      n = n[n.length-1].toLowerCase();
+      $("#" + n + "_count").html(val.length + " restrictions in " + n + " category");
+    }
   });
 }
 
